e216a1667d81d8c2c1ed2d7ae76aeda07b0cb876
Howard Wang <howard.wang@kyperdata.com>
2020-07-23 11:37:31 +0800

    test: add regex printer test cases

--- test/main/regex/RegexPrinterTest.java
+++ test/main/regex/RegexPrinterTest.java
@@ -31,4 +31,49 @@ public class RegexPrinterTest {
         assertNotNull(matches);
         assertEquals(0, matches.size());
     }
+
+    @Test
+    public void testWithGroupName() {
+        // arrange
+        String input = "19年";
+        String patternString = "^" +
+                "(?:(?:19|20)\\d{2} ?|(?:一九|二零)(?:[一二三四五六七八九零]{2}))年" +
+                "(?: ?(?:0?[1-9]|1[012]) ?|一?十[一二]|[一二三四五六七八九十])月" +
+                "(?: ?(?:[0]?[1-9]|[12][0-9]|3[01]) ?|[一二三四五六七八九十]|(?:[一二]?十|廿)[一二三四五六七八九]|(?:三十|卅[一]?))[日號号]" +
+                "(?: ?(?:[01]?[1-9]|2[0-3]|0) ?|[零一二三四五六七八九十]|一?十[一二三四五六七八九]|(?:二十|廿)[一二三]?)[点时點時]" +
+                "$";
+
+        // act
+        List<String> matches = printer.printAllMatches(input, patternString, CASE_INSENSITIVE);
+        System.out.println(matches);
+
+        // assert
+        assertNotNull(matches);
+        assertEquals(0, matches.size());
+    }
+
+    @Test
+    public void testWithGroup() {
+        // arrange
+        String input = "2019年2月1日12时12分59秒";
+        String patternString = "(" +
+                "(?:(?<year>\\d{1,4})年)?(?:的?)" +
+                "(?:第?(?<quarter>\\d)季)?(?:的?)" +
+                "(?:(?<month>\\d{1,2})月(?:的?)(?:第?(?<mweek>\\d)[周週])?)?" +
+                "(?:第?(?<week>\\d{1,2})[周週])?(?:的?)" +
+                "(?:(?<day>\\d{1,2})[日号])?" +
+                "(?:(?<hour>\\d{1,2})[点时點時])?" +
+                "(?:(?<minute>\\d{1,2})分)?" +
+                "(?:(?<second>\\d{1,2})秒)?" +
+                "(?:(?<millisecond>\\d{1,3})毫秒)?" +
+                ")";
+
+        // act
+        List<String> matches = printer.printAllMatches(input, patternString, CASE_INSENSITIVE);
+        System.out.println(matches);
+
+        // assert
+        assertNotNull(matches);
+        assertEquals(0, matches.size());
+    }
 }
