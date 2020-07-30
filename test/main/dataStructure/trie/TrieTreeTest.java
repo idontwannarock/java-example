82299738513b1b622e010d49a5137bb579c2c62b
Howard Wang <howard.wang@kyperdata.com>
2020-07-30 09:41:01 +0800

    test: add auto detect table mechanism to trie tree test

--- test/main/dataStructure/trie/TrieTreeTest.java
+++ test/main/dataStructure/trie/TrieTreeTest.java
@@ -5,11 +5,18 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@SuppressWarnings({"SqlResolve", "SqlNoDataSourceInspection"})
 public class TrieTreeTest {
+
+    private static final String URL = "jdbc:postgresql://***:***/***?currentSchema=public";
+    private static final String USER = "***";
+    private static final String PASSWORD = "***";
 
     private static FirstCharTrieTrees trees;
 
@@ -17,24 +24,68 @@ public class TrieTreeTest {
     public static void init() {
         long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         long start = System.currentTimeMillis();
-
-        trees = new FirstCharTrieTrees();
-        String url = "jdbc:postgresql://**:5432/**?currentSchema=public";
-        @SuppressWarnings({"SqlResolve", "SqlNoDataSourceInspection"})
-        String sql = "SELECT customerno FROM **";
         try {
+            long dataSourceId = 43;
             Class.forName("org.postgresql.Driver");
-            try (Connection connection = DriverManager.getConnection(url, "**", "**");
-                 Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(sql)) {
-                while (resultSet.next()) {
-                    String customerNo = resultSet.getString("customerno");
-                    if (StringUtils.isNotBlank(customerNo)) {
-                        trees.insert(customerNo);
+            try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+                String findDataFrameNameSql = "SELECT name FROM dataframe WHERE datasource_id = ? AND (state = 'Enable' OR state = 'Warn')";
+                List<String> tableNames = new ArrayList<>();
+                try (PreparedStatement ps = connection.prepareStatement(findDataFrameNameSql)) {
+                    ps.setLong(1, dataSourceId);
+                    try (ResultSet resultSet = ps.executeQuery()) {
+                        while (resultSet.next()) {
+                            String tableName = resultSet.getString("name");
+                            if (StringUtils.isNotBlank(tableName)) {
+                                tableNames.add(tableName);
+                            }
+                        }
+                    }
+                }
+                System.out.println("Found table: " + tableNames);
+                if (tableNames.isEmpty()) {
+                    throw new IllegalArgumentException("No table were found");
+                }
+                trees = new FirstCharTrieTrees();
+                for (String tableName : tableNames) {
+                    String findDistinctiveColumnNamesSql = "SELECT dc.name FROM datacolumn dc INNER JOIN dataframe df ON df.id = dc.dataframe_id WHERE dc.active = true AND dc.stats_type = 'CATEGORY' AND (df.state = 'Enable' OR df.state = 'Warn') AND df.datasource_id = ? AND df.name = ? ";
+                    List<String> columnNames = new ArrayList<>();
+                    try (PreparedStatement ps = connection.prepareStatement(findDistinctiveColumnNamesSql)) {
+                        ps.setLong(1, dataSourceId);
+                        ps.setString(2, tableName);
+                        try (ResultSet resultSet = ps.executeQuery()) {
+                            while (resultSet.next()) {
+                                String columnName = resultSet.getString("name");
+                                if (StringUtils.isNotBlank(columnName)) {
+                                    columnNames.add(columnName);
+                                }
+                            }
+                        }
+                    }
+                    if (columnNames.isEmpty()) {
+                        continue;
+                    }
+                    StringBuilder selectDataValueSql = new StringBuilder("SELECT ");
+                    for (int index = 0; index < columnNames.size(); index++) {
+                        if (index != 0) {
+                            selectDataValueSql.append(", ");
+                        }
+                        selectDataValueSql.append(columnNames.get(index));
+                    }
+                    selectDataValueSql.append(" FROM ").append(tableName);
+                    try (Statement statement = connection.createStatement();
+                         ResultSet resultSet = statement.executeQuery(selectDataValueSql.toString())) {
+                        while (resultSet.next()) {
+                            for (String columnName : columnNames) {
+                                String value = resultSet.getString(columnName);
+                                if (StringUtils.isNotBlank(value)) {
+                                    trees.insert(value);
+                                }
+                            }
+                        }
                     }
                 }
             }
-        } catch (ClassNotFoundException | SQLException | NullPointerException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
 
